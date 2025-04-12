5c3c7994ec6767c287b9ad2df93b3e4eeac509cf
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-04-11 19:15:05 -0500

    feat: add navigation buttons for counter, scroll, form, semantics and animated pages in HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,6 +24,41 @@ class HomePage extends StatelessWidget {
               size: 100,
               color: Theme.of(context).colorScheme.primary,
             ),
+            ElevatedButton(
+              key: const Key('button_counter'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.counter);
+              },
+              child: const Text('Ir a la página de Counter'),
+            ),
+            ElevatedButton(
+              key: const Key('button_scroll'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.scroll);
+              },
+              child: const Text('Ir a la página de Scroll'),
+            ),
+            ElevatedButton(
+              key: const Key('button_form'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.form);
+              },
+              child: const Text('Ir a la página de Formulario'),
+            ),
+            ElevatedButton(
+              key: const Key('button_semantics'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.semantics);
+              },
+              child: const Text('Ir a la página de Semantics'),
+            ),
+            ElevatedButton(
+              key: const Key('button_animated'),
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.animated);
+              },
+              child: const Text('Ir a la página de Animaciones'),
+            ),
           ],
         ),
       ),
